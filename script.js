4aed8ec5d722418ef555578a3e88d126f292b366
TiagoPaulin <thiago.vmp15@gmail.com>
2023-07-20 16:19:37 -0300

    gerar tabuleiro

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+function gerarTabuleiro(qtdeCartas){
+    document.write("<table>");
+    if (qtdeCartas == 9) {
+        for (i = 0; i < 3; i ++){
+            document.write("<tr>");
+            for (j = 0; j < 3; j ++){
+                document.write("<td>Oi</td>");
+            }
+            document.write("</tr>");
+        }
+    }
+    if (qtdeCartas == 16) {
+        for (i = 0; i < 4; i ++){
+            document.write("<tr>");
+            for (j = 0; j < 4; j ++){
+                document.write("<td>Oi</td>");
+            }
+            document.write("</tr>");
+        }
+    }
+    if (qtdeCartas == 25) {
+        for (i = 0; i < 5; i ++){
+            document.write("<tr>");
+            for (j = 0; j < 5; j ++){
+                document.write("<td>Oi</td>");
+            }
+            document.write("</tr>");
+        }
+    }
+    document.write("</table>");
+}
+
 function iniciarJogo(){
     var nome = document.getElementById("username");
     var username = nome.value;
@@ -6,5 +38,6 @@ function iniciarJogo(){
     } else {
         var menu = document.getElementById("menu");
         menu.setAttribute("class", "invisible");
+        gerarTabuleiro(9);
     }
 }
